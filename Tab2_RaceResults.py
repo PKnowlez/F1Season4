296d4d7bdad22b7fcaaa1f58b00f74f32faba7bf
Patrick Knowles <pgknowles1@gmail.com>
2025-10-02 13:48:55 -0600

    race result table fix

--- Tab2_RaceResults.py
+++ Tab2_RaceResults.py
@@ -8,19 +8,20 @@ from PIL import Image
 # Expands for each race: Reports race results like post race screen
 def Tab2(races,df,race_place,race_points):
     with st.expander('Preseason: Silverstone'):
+        st.subheader("Winner: " + 'Jayden' + " - " + 'Mercedes')
         preseason = {
             "Place":                ['1','2','3','4','5','6','7','8','9','10','11','12','DNF','DNF/S'],
             "Driver":               ['Jayden','Joshua','Del','Nick','Jairo','Patrick','Eddie','Brently','Travis','Matthew','Erick','Josh','Leo','Boz'],
             "Team":                 ['Mercedes','Alpine','Aston Martin','McLaren','Mercedes','VCARB','Alpine','Red Bull','McLaren','Red Bull','Ferrari','VCARB','Ferrari','Aston Martin'],
             "Qualifying":           ['3','1','5','2','9','4','7','6','12','11','8','10','14','13'],
-            "Fastest Lap":          ['Yes','No','No','No','No','No','No','No','No','No','No','No','No','No'],
-            "Driver of the Day":    ['No','No','No','No','No','No','No','No','No','Yes','No','No','No','No'],
-            "Most Over Takes":      ['No','No','No','No','No','No','No','No','No','No','No','Yes','No','No'],
-            "Cleanest Driver":      ['No','No','Yes','No','No','No','No','No','No','No','No','No','No','No']
+            "Fastest Lap":          ['Yes','-','-','-','-','-','-','-','-','-','-','-','-','-'],
+            "Driver of the Day":    ['-','-','-','-','-','-','-','-','-','Yes','-','-','-','-'],
+            "Most Overtakes":      ['-','-','-','-','-','-','-','-','-','-','-','Yes','-','-'],
+            "Cleanest Driver":      ['-','-','Yes','-','-','-','-','-','-','-','-','-','-','-']
         }
 
         preseason_df = pd.DataFrame(preseason)
-        st.table(preseason_df)     
+        st.dataframe(preseason_df, hide_index=True)     
 
     for i in range(len(races)):
         if i == 0:
@@ -35,12 +36,24 @@ def Tab2(races,df,race_place,race_points):
                     
                     # Construct the correct column name
                     qualifying_col = races[i] + 'Qualifying' 
-                    if '(S)' in races[i]:  # Adjust for Sprint races
+                    if '(S)' in races[i]:
                         qualifying_col = races[i].replace(' (S)', '') + 'SprintQualifying'
 
                     fastestlap_col = races[i] + 'FastestLap'
                     if '(S)' in races[i]:
                         fastestlap_col = races[i].replace(' (S)','') + 'SprintFastestLap'
+
+                    DOTD_col = races[i] + 'DOTD'
+                    if '(S)' in races[i]:
+                        DOTD_col = races[i].replace(' (S)','') + 'SprintDOTD'
+
+                    MOT_col = races[i] + 'MOT'
+                    if '(S)' in races[i]:
+                        MOT_col = races[i].replace(' (S)','') + 'SprintMOT'
+
+                    CD_col = races[i] + 'CD'
+                    if '(S)' in races[i]:
+                        CD_col = races[i].replace(' (S)','') + 'SprintCD'
 
                     race_results_df = pd.DataFrame({
                     'Place': df_sorted[race_place[i-1]],
@@ -48,19 +61,36 @@ def Tab2(races,df,race_place,race_points):
                     'Team': df_sorted['Team'],
                     'Qualifying': df_sorted[qualifying_col],
                     'Points': df_sorted[race_points[i-1]],
-                    'Fastest Lap': df_sorted[fastestlap_col]
+                    'Fastest Lap': df_sorted[fastestlap_col],
+                    'Driver of the Day': df_sorted[DOTD_col],
+                    'Most Overtakes': df_sorted[MOT_col],
+                    'Cleanest Driver': df_sorted[CD_col]
                     })
 
                     race_results_df['Place'] = race_results_df['Place'].replace({
                         21: 'DNF', 
-                        22: 'DNS'
+                        22: 'DNS',
+                        '21': 'DNF', 
+                        '22': 'DNS'
                     })
 
                     race_results_df['Qualifying'] = race_results_df['Qualifying'].replace({
                         21: 'DNF', 
-                        22: 'DNS'
+                        22: 'DNS',
+                        '21': 'DNF', 
+                        '22': 'DNS'
                     })
+                    
+                    loop_columns = ['Fastest Lap','Driver of the Day','Most Overtakes','Cleanest Driver']
 
-                    st.table(race_results_df)
+                    for n in range(len(loop_columns)):
+                        race_results_df[loop_columns[n]] = race_results_df[loop_columns[n]].replace({
+                            'y': 'Yes', 
+                            'n': '-',
+                            'Y': 'Yes',
+                            'N': '-'
+                        })
+
+                    st.dataframe(race_results_df, hide_index=True)
             else:
                 x = 0
