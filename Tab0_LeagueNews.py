137f5b402445378113122b1a5402ff775fede4a6
Patrick Knowles <pgknowles1@gmail.com>
2025-12-01 19:17:07 -0700

    Spa recap

--- Tab0_LeagueNews.py
+++ Tab0_LeagueNews.py
@@ -5,7 +5,7 @@ from Articles import season4_track_overview, season4_schedule_reveal, season4_tr
                 season4_Bahrain_Recap, season4_Miami_Week, season4_Miami_FIA, season4_Miami_Recap, \
                 season4_Spain_Week, season4_Spain_Recap, season4_Spain_FIA, season4_Mexico_Week, season4_Nick_1000, \
                 season4_Mexico_Recap, season4_Baku_Week, season4_Baku_Recap, season4_AustriaR_Week, \
-                season4_AustriaR_Recap, season4_Spa_Week, season4_Spa_FIA
+                season4_AustriaR_Recap, season4_Spa_Week, season4_Spa_FIA, season4_Spa_Recap
 
 def Tab0():
     if 'show_all_content' not in st.session_state:
@@ -13,9 +13,7 @@ def Tab0():
 
     #region --
 
-    season4_Spa_FIA.article()
-    
-    season4_Spa_Week.article()
+    season4_Spa_Recap.article()
 
     #endregion
     
@@ -31,6 +29,10 @@ def Tab0():
             st.rerun()
 
     if st.session_state.show_all_content:
+
+        season4_Spa_FIA.article()
+    
+        season4_Spa_Week.article()
 
         season4_AustriaR_Recap.article()
 
